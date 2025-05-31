61dad729644cafe13b196668ea7256034a7990c5
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2025-05-31 18:23:09 +0530

    used useEffect hooks

--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
 function CoinTable(){
+    async function download(){
+        const response = await fetch("https://api.coingecko.com/api/v3/search/trending");
+        console.log(response);
+
+        const result = response.json();
+        console.log(result)
+    }
+
+    useEffect(()=>{
+        //because the dependency array is empty this component will only run once 
+        //when the component is rendered first time (at mounting)
+        download();
+    },[]);
     return(
         <>
             Coin Table
